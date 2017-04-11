6a573f6287e896ec0cb274e36926b9a512c04896
HudsonAndroid <2216274479@qq.com>
2017-04-11 14:08:19 +0800

    Add files via upload

---
+++ SelectProgressBar/main/java/com/hudson/myrecyclerview/MainActivity.java
@@ -0,0 +1,27 @@
+package com.hudson.myrecyclerview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.audiofx.Visualizer;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
